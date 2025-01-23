7daa4dacb44743a0ef845b5e7faf50deb395eff1
Minami Takayama <m.inam.ina03037@gmail.com>
2025-01-23 17:06:18 +0900

    added resources to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :listings
+  resources :offers
+  resources :dashboard
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  get "listings/:id", to: "listings#show", as: :listing
-  # Defines the root path route ("/")
-  # root "posts#index"
-  get "/dashboard", to: "dashboard#index", as: :dashboard
+#   get "up" => "rails/health#show", as: :rails_health_check
+#   # Defines the root path route ("/")
+#   # root "posts#index"
+
+#   get "/dashboard", to: "dashboard#index", as: :dashboard
 end
